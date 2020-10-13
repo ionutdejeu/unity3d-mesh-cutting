86c778859e724adb13bd8ab23f97e10cc95da38f
ionutdejeu <ionut.dejeu@gmail.com>
2020-10-13 09:37:53 +0300

    Work in progress with learning the math behind meshcutting

--- Assets/DrawBounds.cs
+++ Assets/DrawBounds.cs
@@ -1,20 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class DrawBounds : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public class DrawBounds : MonoBehaviour {
+
+    private MeshFilter filter;
+
+    void OnDrawGizmosSelected()
     {
-        
-    }
+        Gizmos.color = Color.yellow;
+        if (enabled)
+        {
+            if (!filter) filter = GetComponent<MeshFilter>();
+            if (filter.sharedMesh == null) return;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            var modelMatrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.lossyScale);
+            Gizmos.matrix = modelMatrix;
+            Gizmos.DrawWireCube(filter.sharedMesh.bounds.center, filter.sharedMesh.bounds.size);
+        }
     }
-
-     
 }
---
+++ Assets/Step1_SimpleTriangle/GenerateTriangle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class GenerateTriangle : MonoBehaviour
+{
+    Mesh m;
+    Vector3[] verts;
+    int[] triangles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m = GetComponent<MeshFilter>().mesh;
+        verts = new Vector3[] {
+            new Vector3(0,0,0),
+            new Vector3(0,0,10),
+            new Vector3(10,0,0)
+        };
+        triangles = new int[] { 0, 1, 2 };
+        m.vertices = verts;
+        m.triangles = triangles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        foreach(Vector3 vert in verts)
+        {
+            // Draw a yellow sphere at the transform's position
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(vert, 1);
+        }
+    }
+}
